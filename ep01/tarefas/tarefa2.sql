0f0b132b3964ce8ca4823a9f078a20969f2c84cd
jpdsbarbosa <jpdsbarbosa@inf.ufpel.edu.br>
2022-05-09 14:18:32 -0300

    tarefa 2 - 2.0

--- ep01/tarefas/tarefa2.sql
+++ ep01/tarefas/tarefa2.sql
@@ -1,8 +1,7 @@
 -- quantos produtos tem mais de 5 litros
 
-select *,
-    product_length_cm * product_height_cm * product_width_cm / 1000 as produtc_volume
-
+select count(*) as qnt_produtos_mais_5L
+    
 from tb_products
 
 where product_length_cm * product_width_cm * product_height_cm / 1000 > 5
